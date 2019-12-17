86335993e3d48ee998bc57375483b6ef3f13439b
AARSHI <aarora0301@gmail.com>
2019-12-18 03:53:51 +0800

    Create first java annotation

--- Reflections/Annotations/JSONSerializer.java
+++ Reflections/Annotations/JSONSerializer.java
@@ -1,4 +1,44 @@
 package Just_Java.Reflections.Annotations;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Implement Serializer to serialize annotated attributes only
+ */
 public class JSONSerializer {
+
+    /**
+     * Serialize attributes annotated with "@JsonField"
+     *
+     * @param object
+     * @return
+     */
+    public String serialize(Object object) {
+        Map<String, String> map = new HashMap<>();
+        try {
+            Class<?> objectClass = object.getClass();
+            for (Field field : objectClass.getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.isAnnotationPresent(JsonField.class)) {
+                    map.putIfAbsent(field.getName(), (String) field.get(object));
+                }
+            }
+
+        } catch (IllegalAccessException ex) {
+
+        }
+        return toJsonSTring(map);
+    }
+
+    private String toJsonSTring(Map<String, String> map) {
+        String elements = map.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + " : " + entry.getValue())
+                .collect(Collectors.joining(", "));
+        return " { " + elements + " } ";
+
+    }
 }
--- Reflections/Annotations/Main.java
+++ Reflections/Annotations/Main.java
@@ -1,4 +1,12 @@
 package Just_Java.Reflections.Annotations;
 
+        import Just_Java.Reflections.Car;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("Hyundai", "Venue", "2019");
+        JSONSerializer serializer = new JSONSerializer();
+        System.out.println(serializer.serialize(car));
+    }
 }
